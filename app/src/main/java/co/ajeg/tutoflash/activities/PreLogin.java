3a899a65adeae5b01f2952ab6f850dffc7925186
AndresQA <34174822+AndresQA@users.noreply.github.com>
2020-12-03 15:54:01 -0500

    prelogin finish

--- app/src/main/java/co/ajeg/tutoflash/activities/PreLogin.java
+++ app/src/main/java/co/ajeg/tutoflash/activities/PreLogin.java
@@ -2,15 +2,40 @@ package co.ajeg.tutoflash.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import co.ajeg.tutoflash.R;
 
 public class PreLogin extends AppCompatActivity {
+    private Button btn_prelogin_register;
+    private Button btn_prelogin_login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pre_login);
+
+        btn_prelogin_register = findViewById(R.id.btn_prelogin_register);
+        btn_prelogin_login = findViewById(R.id.btn_prelogin_login);
+
+        btn_prelogin_login.setOnClickListener(this::login);
+        btn_prelogin_register.setOnClickListener(this::register);
+
     }
+
+    public void login (View v){
+        Intent inten = new Intent(this, LoginActivity.class);
+        startActivity(inten);
+    }
+
+    public void register (View v){
+        Intent inten = new Intent(this, RegistroActivity.class);
+        startActivity(inten);
+    }
+
+
 }
