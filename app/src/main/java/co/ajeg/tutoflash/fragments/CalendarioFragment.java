88e32a791ab83c47548a5ce3959ccbb4908e44dd
Cristianx10 <42477569+Cristianx10@users.noreply.github.com>
2020-12-08 10:17:37 -0500

    Calendario init values

--- app/src/main/java/co/ajeg/tutoflash/fragments/CalendarioFragment.java
+++ app/src/main/java/co/ajeg/tutoflash/fragments/CalendarioFragment.java
@@ -1,12 +1,16 @@
 package co.ajeg.tutoflash.fragments;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.TextView;
 
 import co.ajeg.tutoflash.R;
@@ -19,6 +23,11 @@ import co.ajeg.tutoflash.fragments.util.FragmentUtil;
  * create an instance of this fragment.
  */
 public class CalendarioFragment extends Fragment {
+
+    TextView tv_calendario_hoy;
+    Button btn_calendario_agregar;
+    CalendarView cv_calendario_calendar;
+    RecyclerView rv_calendario_eventos;
 
 
     public CalendarioFragment() {
@@ -43,10 +52,20 @@ public class CalendarioFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_calendario, container, false);
 
+        this.tv_calendario_hoy = view.findViewById(R.id.tv_calendario_hoy);
+        this.btn_calendario_agregar = view.findViewById(R.id.btn_calendario_agregar);
+        this.cv_calendario_calendar = view.findViewById(R.id.cv_calendario_calendar);
+        this.rv_calendario_eventos = view.findViewById(R.id.rv_calendario_eventos);
+
         mainActivity = FragmentUtil.getActivity();
         mainActivity.headerFragment.changeTitleHeader("Calendario");
 
+        this.btn_calendario_agregar.setOnClickListener(this::agregarEvento);
 
         return view;
+    }
+
+    public void agregarEvento(View v){
+
     }
 }
